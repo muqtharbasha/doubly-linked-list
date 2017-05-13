dbaf50e4fad0f16dd6a1f4d31ec3fa0f1696cbee
Muqthar <muqtharbasha@gmail.com>
2017-05-13 16:41:48 +0530

    double linked list code modified

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,34 +1,28 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {
 
-        this.length = 0;
-        this._head = null;
-        this._tail = null;  
-    }
+  constructor() {
+    this.length = 0;
+    this._head = null;
+    this._tail = null;
+  }
 
-    append(data) {
-    var addInHead = false,
-      addInTail = false;
-    var node = new Node(data);
+  append(data) {
+    var node;
     if (this.length === 0) {
-      this._tail = node;
+      node = new Node(data);
       this._head = node;
-    }
-    if (this.length === 0) {
-      this._head = new Node(data);
-      this.length++
+      this._tail = node;
     } else if (this.length === 1) {
       this._tail = new Node(data, null, this._head);
       this._head.prev = this._tail;
-      this.length++
     } else if (this.length > 1) {
-      var node = new Node(data, null, this._tail)
+      node = new Node(data, null, this._tail)
       this._tail.prev = node;
       this._tail = node;
-      this.length++;
     }
+    this.length++;
     return this;
   }
 
@@ -53,11 +47,11 @@ class LinkedList {
     return node.data;
   }
 
-    insertAt(index, data) {
-    var node = this._head,
-      count = 0;
+  insertAt(index, data) {
+    var node, newnode,count = 0;
+    node = this._head;
     if (index === 0) {
-      var node = new Node(data, this._head, null);
+      node = new Node(data, this._head, null);
       this._head.next = node;
       return this;
     }
@@ -65,7 +59,7 @@ class LinkedList {
       node = node.prev
       count++
     } while (count < index)
-    var newnode = new Node(data, node, node.next);
+    newnode = new Node(data, node, node.next);
     node.next.prev = newnode
     node.next = newnode
     this.length++;
@@ -73,11 +67,7 @@ class LinkedList {
   }
 
   isEmpty() {
-    if (this.length === 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return this.length === 0 ? true : false;
   }
 
   clear() {
@@ -107,14 +97,15 @@ class LinkedList {
   }
 
   reverse() {
-    var node = this._head
+    var node = this._head,
+    n,headData;
     do {
-      var n = node.next;
+       n = node.next;
       node.next = node.prev;
       node.prev = n;
       node = node.next;
     } while (node != null && node.next != null)
-    var headData = this._head;
+    headData = this._head;
     this._head = this._tail;
     this._tail = headData;
     return this;
